3b0e314bc426d76ec1d438e4cbd540d8ec4fd414
Filipe Bellezoni <bellezoni@gmail.com>
2019-02-26 14:21:23 -0300

    Lista de usuários

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -3,19 +3,29 @@ import api from "../../services/api";
 
 export default class Main extends Component {
 
-    componentDidMount(){
+    state = {
+        users: [],
+    };
+
+    componentDidMount() {
         this.loadProducts();
     }
 
     loadProducts = async () => {
         const response = await api.get("/user");
-
-        console.log(response);
+        this.setState({users: response.data.data});
     };
 
-
     render() {
-        return <h1>Hello Filipe</h1>
+        return (
+            <div className="user-list">
+                {
+                    this.state.users.map(user => (
+                        <h2 key={user.id}>{user.name}</h2>
+                    ))
+                }
+            </div>
+        );
     }
-    
+
 }
