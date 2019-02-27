7382b8f45dd29c50aac4b66ca2b6e4adb0d1c750
Filipe Bellezoni <bellezoni@gmail.com>
2019-02-27 00:52:21 -0300

    paginação

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,29 +1,52 @@
 import React, {Component} from "react";
 import api from "../../services/api";
-
+import './styles.css';
 export default class Main extends Component {
 
     state = {
         users: [],
+        usersInfo: {},
     };
 
     componentDidMount() {
         this.loadProducts();
     }
 
-    loadProducts = async () => {
-        const response = await api.get("/user");
-        this.setState({users: response.data.data});
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/user?page=${page}`);
+        const { data, ...usersInfo } = response.data;
+        this.setState({users: data, usersInfo});
+    };
+
+    nextPage = () => {
+        const {current_page, last_page} = this.state.usersInfo;
+        if (current_page === last_page) { return }
+        this.loadProducts(current_page+1)
+    };
+
+    prevPage = () => {
+        const {current_page} = this.state.usersInfo;
+        if (current_page === 1) { return }
+        this.loadProducts(current_page-1)
     };
 
     render() {
+        const { users, usersInfo: {current_page, last_page} } = this.state;
         return (
             <div className="user-list">
                 {
-                    this.state.users.map(user => (
-                        <h2 key={user.id}>{user.name}</h2>
+                    users.map(user => (
+                        <article key={user.id}>
+                            <strong>{user.name}</strong>
+                            <p>{ user.email }</p>
+                            <a href="">Acessar</a>
+                        </article>
                     ))
                 }
+                <div className="actions">
+                    <button disabled={ current_page === 1} onClick={this.prevPage}>Anterior</button>
+                    <button disabled={ current_page === last_page} onClick={this.nextPage}>Próximo</button>
+                </div>
             </div>
         );
     }
